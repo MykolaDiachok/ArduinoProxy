39d753761020cbc5e08b790955f67ec7cee010ad
Mykola Diachok <ndyachok@gmail.com>
2020-11-30 08:24:01 +0200

    removed the sample add swagger

--- ArduinoProxy/Startup.cs
+++ ArduinoProxy/Startup.cs
@@ -25,6 +25,7 @@ namespace ArduinoProxy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen();
             services.AddControllers();
             services.AddMemoryCache();
             services.AddScoped<ConnectToArduino>();
@@ -33,6 +34,11 @@ namespace ArduinoProxy
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Arduino proxy");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
